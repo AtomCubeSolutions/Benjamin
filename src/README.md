f8ea515616bca206659c628d1db38a35698261e4
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-05-26 18:15:27 +0200

    Basic example

---
+++ src/README.md
@@ -0,0 +1,10 @@
+BenjAtomin
+==========
+
+Minimal example how to use botman library with web driver.
+
+See: 
+
+ * https://botman.io/2.0/installation#getting-started
+ * https://botman.io/2.0/web-widget
+
---
+++ src/chat.php
@@ -0,0 +1,28 @@
+<?php
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+
+require_once '../vendor/autoload.php';
+
+$config = [
+        // Your driver-specific configuration
+        // "telegram" => [
+        //    "token" => "TOKEN"
+        // ]
+];
+
+// Load the driver(s) you want to use
+DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
+
+// Create an instance
+$botman = BotManFactory::create($config);
+
+// Give the bot something to listen for.
+$botman->hears('hello', function (BotMan $bot) {
+    $bot->reply('Hello yourself.');
+});
+
+// Start listening
+$botman->listen();
